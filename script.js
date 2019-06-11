6de2f8dd339e33c18f6d5716f32e2568e02ef408
dvdlm <47398685+dvdlm@users.noreply.github.com>
2019-06-11 10:59:38 -0700

    change crosshair to cube face

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-var x,y;
-var theta=0;
-var r1=59;
-var rate = 0.1;
-
 function setup() {
-var cnv = createCanvas(windowWidth, windowHeight);
-background(69);
+var cnv = createCanvas(windowWidth, windowHeight,WEBGL);
 stroke(200);
-noCursor();
+noFill();
 }
 
 function windowResized() {
@@ -16,10 +10,7 @@ resizeCanvas(windowWidth, windowHeight);
 
 function draw() {
 	background(69);
-	line(0,mouseY,windowWidth,mouseY);
-	line(mouseX,0,mouseX,windowHeight);
-	x = mouseX + r1*Math.sin(theta);
-	y = mouseY + r1*-Math.cos(theta);
-	ellipse(x,y,20,20); fill(69)
-	theta = (theta + rate*(abs(sin(theta))+0.1) )% (2*3.14);
+	rotateX(-(mouseY-height/2)/1000);
+    rotateY((mouseX-width/2)/1000);
+    box(windowHeight/3);
 }
