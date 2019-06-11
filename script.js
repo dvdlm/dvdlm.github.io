4ac180a9651b542c2a8e84f4b2f432b7a4ce4a10
dvdlm <47398685+dvdlm@users.noreply.github.com>
2019-06-11 14:32:57 -0700

    spinny cube

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var z = 0;
+
 function setup() {
 var cnv = createCanvas(windowWidth, windowHeight,WEBGL);
 stroke(200);
@@ -13,5 +15,7 @@ function draw() {
 	background(69);
 	rotateX(-(mouseY-height/2)/1000);
     rotateY((mouseX-width/2)/1000);
+	z++;
+	rotateZ(z/100);
     box(windowHeight/3);
 }
