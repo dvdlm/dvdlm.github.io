ff6a0461d105a071cd9e0210be3882f69afd4e5a
dvdlm <47398685+dvdlm@users.noreply.github.com>
2019-06-12 08:11:22 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 var z = 0;
+var x = 0, y = 0;
+let dx,dy;
 
 function setup() {
 var cnv = createCanvas(windowWidth, windowHeight,WEBGL);
@@ -13,8 +15,10 @@ resizeCanvas(windowWidth, windowHeight);
 
 function draw() {
 	background(69);
-	rotateX(-(mouseY-height/2)/1000);
-    rotateY((mouseX-width/2)/1000);
+	dx=(mouseX-x)*0.03;
+	dy=(mouseY-y)*0.03;
+	rotateX(-(dy-height/2)/1000);
+  	rotateY((dx-width/2)/1000);
 	z++;
 	rotateZ(z/3500);
     box(windowHeight/3);
