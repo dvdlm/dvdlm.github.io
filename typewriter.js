a642446ae113481b7576e8268afd1dc74ef0e2d6
David Lim <47398685+dvdlm@users.noreply.github.com>
2023-06-06 19:25:47 -0700

    add secret 1 (try refresh?)

--- typewriter.js
+++ typewriter.js
@@ -3,13 +3,21 @@ let toType;
 let div;
 var m6c=["#9EDBF9","#B689C8","#FFC261","#FDF6AF","#F3B6CF","#5e4fa2"][Math.floor(Math.random() * 6)]; // #ebeff1. Choose random accent colour
 
+if (localStorage.getItem("refreshCount")==null) {
+    refreshCount=0;
+}
+else {
+    refreshCount=localStorage.getItem("refreshCount");
+}
+
 function typewriter() {
     if (div==undefined) {
         div=document.getElementById('myName');
     }
     if (toType==undefined) {
         // toType=div.innerHTML;
-        toType="hi, i'm david. <br   /> i'm a mechanical engineer in BC. <br   /> nice to meet you!";
+        toType=["hi, i'm david. <br   /> i'm a mechanical engineer in BC. <br   /> nice to meet you!",
+        "hi, i'm david. <br   /> i'm an engineer     <br/> but i like making pretty stuff too. <br   /> check this out: "][refreshCount%2]
     }
     
     div.innerHTML=toType.slice(0,i).replace(/(?!<.*?>)<.*/,"").replace(/(?<=i'm )(da?v?i?d?\.?)/,"<span style=\"color:"+m6c+"\">$1</span>") + '_';
@@ -18,7 +26,11 @@ function typewriter() {
     if (i<=toType.length) {
         setTimeout(function() {typewriter()},50);
     }
-    else {loop()};
+    else {
+        if (refreshCount>0) {loop()}; // start drawing
+        refreshCount++;
+        localStorage.setItem("refreshCount",refreshCount);
+    };
 }
 
 window.onload = function() { setTimeout(function(){typewriter()},500) };
